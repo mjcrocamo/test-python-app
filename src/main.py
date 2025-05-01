27a3245c7616760e9e66b66ee9b8cef1ca21398c
Maggie Crocamo <mjcrocamo@gmail.com>
2025-05-01 15:27:18 -0400

    saving

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from .controllers import hello_world_controller
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(hello_world_controller.router)
