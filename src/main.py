94b614d56856892592cee631d0bb2688fca598b2
Maggie Crocamo <mjcrocamo@gmail.com>
2025-05-01 16:33:02 -0400

    adding client

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
-from .controllers import hello_world_controller
+from .controllers import hello_world_controller, cat_facts_controller
 
 app = FastAPI()
 app.include_router(hello_world_controller.router)
+app.include_router(cat_facts_controller.router)
